f104c9312a59c635acf750b2ca706d941b76b120
s0m3body <47750906+s0m3body@users.noreply.github.com>
2025-06-05 22:02:08 +0200

    layout

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def login_page():
     st.markdown("""
         <style>
             div[data-testid="stForm"] {
-                max-width: 600px;
+                max-width: 800px;
                 width: 100%;
             }
         </style>
