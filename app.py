ffbc166bafdc6a31692a30817a55d628e1a1dcc5
s0m3body <47750906+s0m3body@users.noreply.github.com>
2025-06-20 12:16:11 +0200

    switching to 20250619

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def assystente_app():
 
     #ASSISTANT_ID = "asst_BzeO7NF2XnErzF2BLRsuBceB" # 20250527
     #ASSISTANT_ID = "asst_eYS26BK1AXnbRTOECdN1Bvax" # 20250619
-    ASSISTANT_ID = "asst_BzeO7NF2XnErzF2BLRsuBceB"
+    ASSISTANT_ID = "asst_eYS26BK1AXnbRTOECdN1Bvax" # 20250619
 
     if "thread_id" not in st.session_state:
         thread = client.beta.threads.create()
