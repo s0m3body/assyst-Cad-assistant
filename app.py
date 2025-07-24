3dbf1f7968633c4ed2b64af96ae664aaf6b7638b
s0m3body <47750906+s0m3body@users.noreply.github.com>
2025-07-24 17:19:25 +0200

    forgotten...

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ from openai import OpenAI
 import time
 import base64
 
-_dollar_pattern = re.compile(r'(?<!\\)\$')
-
 def get_base64_image(image_path):
     with open(image_path, "rb") as img_file:
         b64_string = base64.b64encode(img_file.read()).decode()
